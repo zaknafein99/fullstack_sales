a683058b99445e0d4a6a61baf72aabe577bc6764
Ismael <imarquez99@gmail.com>
2020-04-13 11:32:28 -0300

    Bootstrap CSS to table

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -3,16 +3,16 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
 
 salesPersonList: SalesPerson[] = [
-  new SalesPerson("Carlos", "Lopez", "carlos.lopez@code.com", 5000),
-  new SalesPerson("John", "Doe", "John.Doe@code.com", 4000),
-  new SalesPerson("Steven", "Chong", "steven.chong@code.com", 9000),
-  new SalesPerson("Anton", "Phake", "anton.phake@code.com", 6000)
+  new SalesPerson("Carlos", "Lopez", "carlos.lopez@code.com", 50000),
+  new SalesPerson("John", "Doe", "John.Doe@code.com", 40000),
+  new SalesPerson("Steven", "Chong", "steven.chong@code.com", 90000),
+  new SalesPerson("Anton", "Phake", "anton.phake@code.com", 60000)
 ];
 
 
