eb1fc6e078028ffe0fe7de47e2a10d7e076e5722
Ismael <imarquez99@gmail.com>
2020-04-17 18:31:27 -0300

    Frontend stub to test backend response

--- backend/spring-boot-ecommerce/src/main/java/com/zaknafein99/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/zaknafein99/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.zaknafein99.ecommerce.dao;
 
 import com.zaknafein99.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin()
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
